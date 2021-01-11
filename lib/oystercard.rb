51f00c120d21606685df0b8009efd14f0e120152
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-11 17:34:15 +0000

    Create #top_up method to load money and update card balance. Added associated unit tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,17 @@
 class OysterCard
     attr_reader :balance
 
+    private
+    attr_writer :balance
+
+    public
+    
     def initialize
-        @balance = 0
+        @balance = 15
     end
+    
+    def top_up(amount)
+        self.balance = balance + amount
+    end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,4 +12,16 @@ describe OysterCard do
 
     end
 
+    describe '#top_up' do
+
+        it { is_expected.to respond_to(:top_up).with(1).argument }
+
+        it 'increases the balance with the amount passed as an argument' do
+            card = OysterCard.new
+            card.top_up(15)
+            expect(card.balance).to eq 15
+        end
+
+    end
+
 end
