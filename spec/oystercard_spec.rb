324f61bbbcf11eec965e064c6391491b7ce177af
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-11 16:28:45 +0000

    OysterCard class created and balance instance variable created with zero balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
-describe OysterCard
+require 'oystercard'
+
+describe OysterCard do
+
+    it { is_expected.to respond_to(:balance) }
+
+    describe 'initialization' do
+
+        it 'has a numeric balance' do
+            expect(subject.balance).to be_an(Numeric)
+        end
+
+    end
 
 end
